20f3a1187ddce3b24c7e5e8068ddc50b6aab97ab
arpitasaraf <arpitasaraf2@gmail.com>
2025-02-10 10:02:56 +0530

    Add files via upload

---
+++ 002-printing-multiple-values.py
@@ -0,0 +1,6 @@
+myname = "smriti"
+collage = 'MU'
+print(myname , collage)
+
+print("my name is",myname,". Collage name is",collage)
+print("my name is"  + myname + " . collage name is " +  collage)
