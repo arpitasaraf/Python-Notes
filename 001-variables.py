86e05683d277738508d4333cc02d286ace243e55
arpitasaraf <arpitasaraf2@gmail.com>
2025-02-09 21:13:37 -0700

    Add files via upload

---
+++ 001-variables.py
@@ -0,0 +1,23 @@
+# Integer
+b = 26
+q = 25 + 25
+print(b)
+print(q)
+
+# Float
+ar = 7.52
+print(ar)
+
+# String 
+name = "aradhya"
+myname ='navansh'
+print(name)
+print(myname)
+
+# Boolean 
+bl = True
+bn = False
+print(bl)
+print(bn)
+
+
