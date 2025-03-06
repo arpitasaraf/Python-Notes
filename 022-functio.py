2806585523095a35ca51c620a0ee0bab7ebfea5e
arpitasaraf <arpitasaraf2@gmail.com>
2025-03-05 21:12:59 -0700

    Add files via upload

---
+++ 022-functio.py
@@ -0,0 +1,22 @@
+# 
+def hello():
+    print("hello")
+hello()
+
+# single variable
+def hello(n1):
+    print(n1)
+hello(26)
+
+# list
+def arr(list):
+    print(list)
+arr([3,5,6,'h4lo'])
+
+# 
+def arr(list):
+    print(list)
+    for i in list:
+        print(i)
+    
+arr([2,4,6,8,10])
---
+++ 023-dictionary-Ques.py
@@ -0,0 +1,11 @@
+#
+dict = {
+    "username":"karishma",
+    "password":"drthjika",
+    "isloggedIn":False
+}
+# print(dict)
+if (dict["isloggedIn"]):
+        print("welcome")
+else:
+        print("Please Log in.")
