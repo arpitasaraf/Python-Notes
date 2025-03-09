c49e99956ee9f42591ac9523eebc361e5806adc5
arpitasaraf <arpitasaraf2@gmail.com>
2025-03-09 23:38:29 +0530

    Update 023-dictionary-Ques.py

--- 023-dictionary-Ques.py
+++ 023-dictionary-Ques.py
@@ -1,4 +1,5 @@
-#
+# 1) WAP to create an object account with properties username , password and isLoggedIn . write an if-else statement to check: 
+     # If isLoggedIn is true , print" Welcome,". Otherwise , print "Please log in."
 dict = {
     "username":"karishma",
     "password":"drthjika",
@@ -9,3 +10,19 @@ if (dict["isloggedIn"]):
         print("welcome")
 else:
         print("Please Log in.")
+print("-------------------------------------------------------------------------------------------------------------------------
+
+# 2) WAP to create an object product with properties name, catergory, and in Stock. 
+    # Use an if-else condition to check if instock is false.
+    # if yes , print is Out of Stock:,otherwise print :avaiable",
+dict = {
+    "name": "manvi",
+    "category":"electronics",
+    "inStock":False
+}
+if not product["inStock"]:
+    print(f'{product["name"]} is Out of stock')
+else:
+    print(f'{product["name"]} is available')
+print("------------------------------------------------------")
+
