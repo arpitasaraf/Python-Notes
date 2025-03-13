1e7f7f335e1017cc1ba35a8d01b67e5da96c24b0
arpitasaraf <arpitasaraf2@gmail.com>
2025-03-12 21:11:49 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,14 @@
+email = 'hello@gmail.com'
+
+list = [1,2,3,False,None]
+
+def sayHello():
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    
+def sum_num(a,b):
+    return a + b 
+
---
+++ sayuri.py
@@ -0,0 +1,14 @@
+def login():
+    print("login")
+
+def add_no(w,q):
+    return w + q
+
+list = [5 , 10 ,15,20,25]
+
+dict = {
+    "name":"smriti",
+    "email":"smriti@22gamil.com"
+} 
+
+tuple = (3,6,9)
