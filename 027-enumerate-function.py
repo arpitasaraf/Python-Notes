edacba3dc92934ca23d43e0c381695b0262c1702
arpitasaraf <arpitasaraf2@gmail.com>
2025-03-19 21:33:28 -0700

    Add files via upload

---
+++ 027-enumerate-function.py
@@ -0,0 +1,42 @@
+l = ['Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry','Lichi']
+
+# output
+# index = 0 , fruit = Apple
+# index = 1 , fruit = Mango
+# index = 2 , fruit = Banana
+# index = 3 , fruit = Cherry
+# index = 4 , fruit = Lichi
+# index = 5 , fruit = Apple
+
+# print(l[0:1])
+# print(l[1:2])
+# print(l[2:3])
+# print(l[3:4])
+# print(l[4:5])
+
+# print('-----------------------')
+
+# print(0 , l[0:1])
+# print(1 , l[1:2])
+# print(2 , l[2:3])
+# print(3 , l[3:4])
+# print(4 , l[4:5])
+
+# print("--------------------------")
+
+print('index = 0' ,'fruit = Apple')
+print('index = 1' , 'fruit = Mango')
+print('index = 2', 'fruit = Banana')
+print('index = 3' , 'fruit = Cherry')
+print('index = 4' , 'fruit = Lichi')
+print("------------------------------------------")
+
+for i in l:
+    for fruit in l:
+        print(fruit)
+    print(i)
+    print('index = 0' ,'fruit = Apple')
+
+
+    
+    
