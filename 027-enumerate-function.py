7296dd64b88235328b5b0abcba457f0b958b9ed8
arpitasaraf <arpitasaraf2@gmail.com>
2025-03-21 06:40:47 +0530

    Update 027-enumerate-function.py

--- 027-enumerate-function.py
+++ 027-enumerate-function.py
@@ -8,22 +8,6 @@ l = ['Apple','Mango','Banana','Cherry','Lichi','Apple','Mango','Banana','Cherry'
 # index = 4 , fruit = Lichi
 # index = 5 , fruit = Apple
 
-# print(l[0:1])
-# print(l[1:2])
-# print(l[2:3])
-# print(l[3:4])
-# print(l[4:5])
-
-# print('-----------------------')
-
-# print(0 , l[0:1])
-# print(1 , l[1:2])
-# print(2 , l[2:3])
-# print(3 , l[3:4])
-# print(4 , l[4:5])
-
-# print("--------------------------")
-
 print('index = 0' ,'fruit = Apple')
 print('index = 1' , 'fruit = Mango')
 print('index = 2', 'fruit = Banana')
@@ -31,11 +15,15 @@ print('index = 3' , 'fruit = Cherry')
 print('index = 4' , 'fruit = Lichi')
 print("------------------------------------------")
 
-for i in l:
-    for fruit in l:
-        print(fruit)
-    print(i)
-    print('index = 0' ,'fruit = Apple')
+-------------------------------")
+
+for index , fruit in enumerate(l):
+    print(index , fruit)
+
+print("--------------------------------------------------------------------------")
+
+for index , fruit in enumerate(l):
+    print(f'index = {index}  fruit = {fruit}')
 
 
     
